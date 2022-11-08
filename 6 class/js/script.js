ebcd89b9e14f652a92ccf7bc8c0e3bf9619c2ca9
Shaxzod1234567 <113818202+Shaxzod1234567@users.noreply.github.com>
2022-11-08 20:17:56 +0500

    Add files via upload

---
+++ 6 class/js/script.js
@@ -0,0 +1,16 @@
+ alert("Вы соглашаетесь правами кондефициальность")
+
+document.addEventListener("mousemove" , parallax)
+
+function parallax(e) {
+    this.querySelectorAll(".img").forEach(img => {
+        const speed = img.getAttribute("data-speed");
+
+        const x = (window.innerWidth - e.pageX * speed) / 100;
+        const y = (window.innerHeight - e.pageY * speed) / 100;
+
+        img.style.transform = `translateX(${x}px) 
+        translateY(${y}px)`;
+
+    });
+}
